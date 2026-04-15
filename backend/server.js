da933a7ee974df6b6352a769ddfee73afa7c0e87
Shlesha5847 <147795373+Shlesha5847@users.noreply.github.com>
2026-04-15 12:43:58 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -89,7 +89,7 @@ app.post("/login", async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
