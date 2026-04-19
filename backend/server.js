188bfc536d860bdccf451f26db3d2b7f127b8374
Tanishka Jain <jain.tanishka0109@gmail.com>
2026-04-19 12:42:05 +0530

    Fix catch-all route for Express

--- backend/server.js
+++ backend/server.js
@@ -171,7 +171,7 @@ app.get('/logout', (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get(/.*/, (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
